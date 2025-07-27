931a41a2ac045f726cb025e3859aa2cd9b47afe3
Dagim Zemene <Daguzemene@gmail.com>
2025-07-27 14:46:29 +0300

    2nd

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,9 +1,9 @@
 
 def display_menu():
     print("Shopping List Manager")
-    print("1. Add item to shopping list")
-    print("2. Remove item from shopping list")
-    print("3. View shopping list")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
     print("4. Exit")
 def main():
     shopping_list = []
