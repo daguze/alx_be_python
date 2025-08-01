4ced4eb8061817d1197d1a71630af99437dfa2a1
Dagim Zemene <Daguzemene@gmail.com>
2025-08-01 19:25:58 +0300

    first try

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+        self.__account_balance = initial_balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return f"deposited {amount}, new balance is {self.__account_balance}"
+        else:
+            return "Deposit amount must be positive"
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"current balance is {self.__account_balance}")
