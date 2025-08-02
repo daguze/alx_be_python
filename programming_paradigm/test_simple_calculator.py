068ecfe0db4ed7609f058373de01098529f6c9ee
Dagim Zemene <Daguzemene@gmail.com>
2025-08-02 20:03:15 +0300

    1

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,28 @@
+import unittest
+from simple_calculator import SimpleCalculator
+class TestSimpleCalculator(unittest.TestCase):
+    """Unit tests for the SimpleCalculator class."""
+
+    def setUp(self):
+        """Create an instance of SimpleCalculator before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_subtract(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+
+    def test_multiply(self):
+        """Test the multiplication method."""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+
+    def test_divide(self):
+        """Test the division method."""
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero should return None
