1e5dfa54a27255f4d1c2a14160cdf91022f7445e
Dagim Zemene <Daguzemene@gmail.com>
2025-08-10 17:12:18 +0300

    2nd

--- oop/library_system.py
+++ oop/library_system.py
@@ -11,7 +11,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"E-Book: {self.title} by {self.author}, File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 class PrintBook(Book):
     def __init__(self, title :str ,author :str ,page_count :int):
         super().__init__(title, author)
